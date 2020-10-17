7ea650b14efca0038d78b97392e58afb0650df03
Kavin Kumar <kavinkumar@Kavins-MacBook-Pro.local>
2020-10-17 18:26:12 +0530

    Added sample test

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class TestDemo {
+
+    @Test
+    public void sampleTest()
+    {
+    assertThat(1,is(1));
+    }
+}
